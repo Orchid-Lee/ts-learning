f980d3065358b5a956fc275c905e55e739427773
Lee <www.zhangxinjie@gmail.com>
2025-08-26 06:53:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 'fuck u';
+let b;
+let c;
+a = 'hello';
+b = -99;
+c = true;
+function count(x, y) {
+    return x + y;
+}
+let result = count(1, 2);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,14 @@
+let a: string = 'fuck u'
+let b: number 
+let c: boolean
+
+a = 'hello'
+b = -99
+c = true
+
+function count(x: number, y: number){
+    return x + y
+}
+
+let result = count(1, 2)
+console.log(result)
